00f91a7f6773a047950cb90d9f35680fe9fcb7b3
Matt <matt.beedle@1000jobboersen.de>
2010-05-03 19:14:01 +0200

    Added mongo 0.19.1 gem dependency

--- config/environment.rb
+++ config/environment.rb
@@ -23,7 +23,7 @@ Rails::Initializer.run do |config|
   config.gem 'inherited_resources', :version => '1.0.3'
   config.gem 'warden'
   config.gem 'devise'
-  #config.gem 'mongo'
+  config.gem 'mongo', :version => '0.19.1'
   config.gem 'carrierwave'
   config.gem 'compass', :version => "0.10.0.pre5", :source => "http://gems.github.com"
   config.gem 'shoulda'
