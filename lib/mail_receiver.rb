0f8f5e52c33babd2a29dad11c91945fc88fe546e
Matt <matt.beedle@1000jobboersen.de>
2010-05-11 11:32:00 +0200

    Fixed mail receiver to use mongo yml file, as it had broken since adding database authentication

--- lib/mail_receiver.rb
+++ lib/mail_receiver.rb
@@ -5,7 +5,19 @@ require 'beanstalk-client'
 require 'mongo_mapper'
 require File.join(File.dirname(__FILE__), '..', 'app', 'models', 'mail_queue')
 
-MongoMapper.database = 'salesflip_production'
+begin
+  db_config = YAML::load(File.read(File.join(File.dirname(__FILE__), '..', 'config', 'mongodb.yml')))
+rescue
+  raise IOError, 'config/mongodb.yml could not be loaded'
+end
+
+mongo = db_config['production']
+MongoMapper.connection = Mongo::Connection.new(mongo['host'] || 'localhost',
+                                               mongo['port'] || 27017)
+MongoMapper.database = mongo['database']
+if mongo['username'] && mongo['password']
+  MongoMapper.database.authenticate(mongo['username'], mongo['password'])
+end
 
 message = $stdin.read
 mail = Mail.new(message)
